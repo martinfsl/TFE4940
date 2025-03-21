2ab89c649a90e2035c840b9d15e0efbdc1312a90
martinfsl <74840025+martinfsl@users.noreply.github.com>
2025-03-21 14:19:29 +0100

    #15 updated FH object to ensure that PRNGs stay in sync

--- GPU/PRN_FREQUENCY_HOPPING/MULTIPLE_RECEIVE_CHANNELS/PPO_PREDICTIVE/fh_pattern.py
+++ GPU/PRN_FREQUENCY_HOPPING/MULTIPLE_RECEIVE_CHANNELS/PPO_PREDICTIVE/fh_pattern.py
@@ -12,20 +12,32 @@ class FH_Pattern:
         self.seeds = [616, 52, 218]
         self.prng_objects = [random.Random(seed) for seed in self.seeds]
 
+    # Generate a sequence of random numbers from all PRNG objects to keep them in sync,
+    # but only return the one corresponding to seed_index.
     def generate_sequence(self, seed_index):
-        prng_object = self.prng_objects[seed_index]
-        return torch.tensor([prng_object.randint(0, NUM_CHANNELS-1) for i in range(self.L)], device = self.device)
+        sequences = []
+        for prng in self.prng_objects:
+            seq = [prng.randint(0, NUM_CHANNELS - 1) for _ in range(self.L)]
+            sequences.append(seq)
+        return torch.tensor(sequences[seed_index], device=self.device)
 
-    # def get_pattern(self, index):
-        # return self.patterns[index]
-    
-    # def print_patterns(self):
-    #     for i in range(self.M):
-    #         print("Pattern ", i, ": ", self.patterns[i])
+    # def generate_sequence(self, seed_index):
+    #     prng_object = self.prng_objects[seed_index]
+    #     return torch.tensor([prng_object.randint(0, NUM_CHANNELS-1) for i in range(self.L)], device = self.device)
 
 if __name__ == '__main__':
-    fh = FH_Pattern()
-    print("Seeds: ", fh.seeds)
-    print("Seed 1: ", fh.generate_sequence(0))
-    print("Seed 2: ", fh.generate_sequence(1))
-    print("Seed 3: ", fh.generate_sequence(2))
+    fh1 = FH_Pattern()
+    fh2 = FH_Pattern()
+    print("Seeds: ", fh1.seeds)
+    print("Time step 1")
+    print("fh1, seed 1: ", fh1.generate_sequence(0))
+    print("fh2, seed 2: ", fh2.generate_sequence(1))
+    print("Time step 2")
+    print("fh1, seed 2: ", fh1.generate_sequence(1))
+    print("fh2, seed 1: ", fh2.generate_sequence(0))
+    print("Time step 3")
+    print("fh1, seed 2: ", fh1.generate_sequence(1))
+    print("fh2, seed 1: ", fh2.generate_sequence(0))
+    print("Time step 4")
+    print("fh1, seed 1: ", fh1.generate_sequence(0))
+    print("fh2, seed 1: ", fh2.generate_sequence(0))
