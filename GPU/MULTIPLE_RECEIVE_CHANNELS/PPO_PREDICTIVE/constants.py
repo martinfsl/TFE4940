Changed non-FH implementation to also consider the correct logprob for multiple receive channels

GPU/MULTIPLE_RECEIVE_CHANNELS/PPO_PREDICTIVE/constants.py
@@ -4,13 +4,9 @@ import numpy as np
 ### Defining constants
 #################################################################################
 
-NUM_CHANNELS = 20
-NUM_USERS = 1
-ACTIONS = np.arange(0, NUM_CHANNELS, 1)
+NUM_CHANNELS = 20 # Number of channels in the system
 NUM_EXTRA_ACTIONS = 5 # Number of extra channels that the Tx and Rx can sense
 NUM_RECEIVE = 3 # Number of channels that Rx receives on simultaneously
-
-channel = [0]*NUM_CHANNELS
 
 # Hyperparameters
 LEARNING_RATE = 0.001
@@ -26,7 +22,6 @@ T = 100 # Number of steps between each update, i.e. length of the trajectory
 M = 20 # Size of mini-batch during training
 K = 15 # Number of epochs
 C1 = 0.5 # Coefficient for the value loss
-# C2 = 0.01 # Coefficient for the entropy loss
 C2 = 0.05 # Coefficient for the entropy loss
 
 # Parameters
@@ -43,14 +38,11 @@ REWARD_INTERFERENCE = -1
 REWARD_UNSUCCESSFUL = -1
 
 # Number of episodes for training
-# NUM_EPISODES = 500
-# NUM_EPISODES = 20000
-# NUM_EPISODES = 50000
-NUM_EPISODES = 100000
+NUM_EPISODES = 1000
+# NUM_EPISODES = 100000
 
 # Number of runs for testing
-NUM_TEST_RUNS = 10000
-# NUM_TEST_RUNS = 50000
+NUM_TEST_RUNS = 1000
 # NUM_TEST_RUNS = 100000
 
 # Bool variable to decide whether fading is to be considered
@@ -80,11 +72,23 @@ TRANSITION_3 = 0.02
 
 # Number of sensed channels
 # How many channels the Tx and Rx can sense at a time (including the channel they are on)
-# NUM_SENSE_CHANNELS = 15
 NUM_SENSE_CHANNELS = 7 # This allows the Tx and Rx to sense all channels
 NUM_JAMMER_SENSE_CHANNELS = 7 # Number of channels the jammer can sense
 
-REWARD_SENSE = 0.5 # Additional reward for being able to sense the other agent's action even though it did receive the message
+REWARD_SENSE = 0 # Additional reward for being able to sense the other agent's action even though it did receive the message
 PENALTY_NONDIVERSE = 0 # Penalty for staying on the same channels for NUM_PREV_ACTIONS episodes
 REWARD_DIVERSE = 0 # Reward for choosing a channel that has not been used in the past NUM_PREV_ACTIONS episodes
-NUM_PREV_ACTIONS = 2 # Number of previous actions to consider for the penalty
+NUM_PREV_ACTIONS = 0 # Number of previous actions to consider for the penalty
+
+STATE_SPACE_SIZE = NUM_SENSE_CHANNELS + 1
+#################################################################################
+### Defining inputs and outputs for the neural networks
+PREDICTION_NETWORK_INPUT_SIZE = STATE_SPACE_SIZE
+USE_PREDICTION = True
+if USE_PREDICTION:
+    PPO_NETWORK_INPUT_SIZE = STATE_SPACE_SIZE + NUM_CHANNELS
+else:
+    PPO_NETWORK_INPUT_SIZE = STATE_SPACE_SIZE
+
+JAMMER_PPO_NETWORK_INPUT_SIZE = NUM_JAMMER_SENSE_CHANNELS + 1
+#################################################################################

GPU/MULTIPLE_RECEIVE_CHANNELS/PPO_PREDICTIVE/tx_agent.py
@@ -21,7 +21,7 @@ class txPredNN(nn.Module):
     def __init__(self):
         super(txPredNN, self).__init__()
 
-        self.input_size = NUM_SENSE_CHANNELS + 1
+        self.input_size = STATE_SPACE_SIZE
         self.hidden_size1 = 128
         self.hidden_size2 = 64
         self.output_size = NUM_CHANNELS
@@ -57,7 +57,7 @@ class txPredNNAgent:
 
         self.device = device
 
-        self.memory_state = torch.empty((0, NUM_SENSE_CHANNELS+1), device=self.device)
+        self.memory_state = torch.empty((0, STATE_SPACE_SIZE), device=self.device)
         self.memory_action = torch.empty((0, 1), device=self.device)
 
         self.pred_network = txPredNN()
@@ -99,8 +99,7 @@ class txPPOActor(nn.Module):
     def __init__(self, device = "cpu"):
         super(txPPOActor, self).__init__()
 
-        self.input_size = NUM_SENSE_CHANNELS + 1 + NUM_CHANNELS
-        # self.input_size = NUM_SENSE_CHANNELS + 1
+        self.input_size = PPO_NETWORK_INPUT_SIZE
         self.hidden_size1 = 128
         self.hidden_size2 = 64
         self.output_size = NUM_CHANNELS
@@ -125,8 +124,7 @@ class txPPOCritic(nn.Module):
     def __init__(self, device = "cpu"):
         super(txPPOCritic, self).__init__()
 
-        self.input_size = NUM_SENSE_CHANNELS + 1 + NUM_CHANNELS
-        # self.input_size = NUM_SENSE_CHANNELS + 1
+        self.input_size = PPO_NETWORK_INPUT_SIZE
         self.hidden_size1 = 128
         self.hidden_size2 = 64
         self.output_size = 1
@@ -170,7 +168,7 @@ class txPPOAgent:
         self.device = device
 
         # PPO on-policy storage (use lists to store one episode/trajectory)
-        self.memory_state = torch.empty((0, NUM_SENSE_CHANNELS+1+NUM_CHANNELS), device=self.device)
+        self.memory_state = torch.empty((0, PPO_NETWORK_INPUT_SIZE), device=self.device)
         self.memory_action = torch.empty((0, 1), device=self.device)
         self.memory_logprob = torch.empty((0, 1), device=self.device)
         self.memory_reward = torch.empty((0, 1), device=self.device)
@@ -208,7 +206,7 @@ class txPPOAgent:
             self.previous_actions = self.previous_actions[1:]
 
     def clear_memory(self):
-        self.memory_state = torch.empty((0, NUM_SENSE_CHANNELS+1+NUM_CHANNELS), device=self.device)
+        self.memory_state = torch.empty((0, PPO_NETWORK_INPUT_SIZE), device=self.device)
         self.memory_action = torch.empty((0, 1), device=self.device)
         self.memory_logprob = torch.empty((0, 1), device=self.device)
         self.memory_reward = torch.empty((0, 1), device=self.device)
@@ -269,16 +267,14 @@ class txPPOAgent:
             values = self.get_value(observation)
 
         # Extract the most probable action as main action and NUM_EXTRA_ACTIONS additional actions which are the next most probable actions
-        main_action = torch.argmax(policy)
+        action = torch.argmax(policy).unsqueeze(0)
         additional_actions = torch.argsort(policy, descending=True)[1:NUM_EXTRA_ACTIONS+1]
 
-        actions = torch.cat((torch.tensor([main_action], device=self.device), additional_actions))
+        action_logprob = torch.log(torch.gather(policy, 0, action))
 
-        action_logprob = torch.log(torch.gather(policy, 0, main_action.unsqueeze(0)))
+        self.channels_selected = torch.cat((self.channels_selected, action))
 
-        self.channels_selected = torch.cat((self.channels_selected, main_action.unsqueeze(0)))
-
-        return actions, action_logprob, values
+        return action, action_logprob, values, additional_actions
     
     # Compute the returns for each time step in the trajectory
     def compute_returns(self):
@@ -311,39 +307,6 @@ class txPPOAgent:
         return advantages
 
     def update(self):
-        returns = self.compute_returns()
-        advantages = self.compute_advantages(returns, self.memory_value)
-
-        values = self.get_value(self.memory_state)
-
-        old_logits = self.actor_network_old(self.memory_state).detach()
-        old_logprobs = torch.log(torch.gather(nn.Softmax(dim=1)(old_logits), 1, self.memory_action.long()))
-
-        new_logits = self.actor_network(self.memory_state)
-        new_logprobs = torch.log(torch.gather(nn.Softmax(dim=1)(new_logits), 1, self.memory_action.long()))
-
-        ratio = torch.exp(new_logprobs - old_logprobs)
-
-        surr1 = ratio * advantages
-        surr2 = torch.clamp(ratio, 1-self.epsilon_clip, 1+self.epsilon_clip)*advantages
-
-        actor_loss = -torch.min(surr1, surr2).mean()
-        critic_loss = nn.MSELoss()(values, returns)
-        total_loss = actor_loss + critic_loss
-
-        self.actor_network_old.load_state_dict(self.actor_network.state_dict()) # Update the weights of the old network to the current network after each update
-
-        self.actor_optimizer.zero_grad()
-        self.critic_optimizer.zero_grad()
-        # actor_loss.backward()
-        # critic_loss.backward()
-        total_loss.backward()
-        self.actor_optimizer.step()
-        self.critic_optimizer.step()
-
-        self.clear_memory()
-
-    def update_epochs_random(self):
         returns = self.compute_returns()
         advantages = self.compute_advantages(returns, self.memory_value)
 
@@ -395,55 +358,3 @@ class txPPOAgent:
 
         self.actor_losses = torch.cat((self.actor_losses, actor_loss.unsqueeze(0)))
         self.critic_losses = torch.cat((self.critic_losses, critic_loss.unsqueeze(0)))
-
-    def update_epochs_sequential(self):
-        returns = self.compute_returns()
-        advantages = self.compute_advantages(returns, self.memory_value)
-
-        values = self.get_value(self.memory_state)
-
-        old_logits = self.actor_network_old(self.memory_state).detach()
-        old_logprobs = torch.log(torch.gather(nn.Softmax(dim=1)(old_logits), 1, self.memory_action.long()))
-
-        data_size = self.memory_state.size(0)
-
-        for epoch in range(self.k):
-            # Instead of shuffling randomly, we process consecutive sequences (episodes) as batches.
-            num_batches = data_size // self.m
-            for _ in range(num_batches):
-                start = random.randint(0, data_size - self.m)
-                end = start + self.m
-                batch_state = self.memory_state[start:end]
-                batch_action = self.memory_action[start:end]
-                batch_logprob = self.memory_logprob[start:end]
-                batch_return = returns[start:end].detach()
-                batch_advantage = advantages[start:end].detach()
-
-                new_logits = self.actor_network(batch_state)
-                new_policy = nn.Softmax(dim=1)(new_logits)
-                new_logprobs = torch.log(torch.gather(new_policy, 1, batch_action.long()))
-                
-                new_dist = torch.distributions.Categorical(new_policy)
-                new_entropy = new_dist.entropy().mean()
-
-                ratio = torch.exp(new_logprobs - batch_logprob)
-
-                surr1 = ratio * batch_advantage
-                surr2 = torch.clamp(ratio, 1 - self.epsilon_clip, 1 + self.epsilon_clip) * batch_advantage
-
-                # actor_loss = -torch.min(surr1, surr2).mean()
-                actor_loss = -torch.min(surr1, surr2).mean() - self.c2*new_entropy
-
-                batch_value = self.get_value(batch_state)
-                critic_loss = nn.MSELoss()(batch_value, batch_return)
-
-                total_loss = actor_loss + self.c1*critic_loss
-
-                self.actor_optimizer.zero_grad()
-                self.critic_optimizer.zero_grad()
-                total_loss.backward()
-                self.actor_optimizer.step()
-                self.critic_optimizer.step()
-
-        self.actor_network_old.load_state_dict(self.actor_network.state_dict())
-        self.clear_memory()