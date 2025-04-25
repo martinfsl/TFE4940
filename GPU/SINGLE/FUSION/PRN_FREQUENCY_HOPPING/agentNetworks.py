8968d35589ad0dac8908ac1d36cc6864cd76ea3c
martifsl-ssh <martin.f.slettom@ntnu.no>
2025-04-25 14:49:21 +0200

    Added a fusion network for testing

---
+++ GPU/SINGLE/FUSION/PRN_FREQUENCY_HOPPING/agentNetworks.py
@@ -0,0 +1,214 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import random
+import numpy as np
+
+import copy
+
+from constants import *
+from fh_pattern import FH_Pattern
+    
+#################################################################################
+### Fusion Model
+#################################################################################
+
+# class ActorNetwork(nn.Module):
+#     def __init__(self):
+#         super(ActorNetwork, self).__init__()
+
+#         self.input_size = PPO_NETWORK_INPUT_SIZE
+#         self.hidden_size1 = 128
+#         self.hidden_size2 = 64
+#         self.output_size = PPO_NETWORK_OUTPUT_SIZE
+
+#         # Defining the fully connected layers
+#         if USE_PREDICTION and USE_STANDALONE_BELIEF_MODULE:
+#             self.fc1 = nn.Linear(self.input_size + PREDICTION_NETWORK_OUTPUT_SIZE, self.hidden_size1)
+#         elif USE_PREDICTION and not USE_STANDALONE_BELIEF_MODULE:
+#             self.fc1 = nn.Linear(self.input_size + P, self.hidden_size1)
+#         else:
+#             self.fc1 = nn.Linear(self.input_size, self.hidden_size1)
+#         self.dropout1 = nn.Dropout(0.3)
+#         self.fc2 = nn.Linear(self.hidden_size1, self.hidden_size2)
+#         self.dropout2 = nn.Dropout(0.3)
+#         self.fc3 = nn.Linear(self.hidden_size2, self.output_size)
+
+#     def forward(self, obs, encoded_belief, dimension=0):
+#         if USE_PREDICTION:
+#             fused = torch.cat([obs, encoded_belief], dim=dimension)
+#         else:
+#             fused = obs
+#         x = torch.relu(self.fc1(fused))
+#         x = self.dropout1(x)
+#         x = torch.relu(self.fc2(x))
+#         x = self.dropout2(x)
+#         x = self.fc3(x)
+
+#         return x
+    
+# class CriticNetwork(nn.Module):
+#     def __init__(self):
+#         super(CriticNetwork, self).__init__()
+
+#         self.input_size = PPO_NETWORK_INPUT_SIZE
+#         self.hidden_size1 = 128
+#         self.hidden_size2 = 64
+#         self.output_size = 1
+
+#         # Defining the fully connected layers
+#         if USE_PREDICTION and USE_STANDALONE_BELIEF_MODULE:
+#             self.fc1 = nn.Linear(self.input_size + PREDICTION_NETWORK_OUTPUT_SIZE, self.hidden_size1)
+#         elif USE_PREDICTION and not USE_STANDALONE_BELIEF_MODULE:
+#             self.fc1 = nn.Linear(self.input_size + P, self.hidden_size1)
+#         else:
+#             self.fc1 = nn.Linear(self.input_size, self.hidden_size1)
+#         self.dropout1 = nn.Dropout(0.3)
+#         self.fc2 = nn.Linear(self.hidden_size1, self.hidden_size2)
+#         self.dropout2 = nn.Dropout(0.3)
+#         self.fc3 = nn.Linear(self.hidden_size2, self.output_size)
+
+#     def forward(self, obs, encoded_belief, dimension=0):
+#         if USE_PREDICTION:
+#             fused = torch.cat([obs, encoded_belief], dim=dimension)
+#         else:
+#             fused = obs
+#         x = torch.relu(self.fc1(fused))
+#         x = self.dropout1(x)
+#         x = torch.relu(self.fc2(x))
+#         x = self.dropout2(x)
+#         x = self.fc3(x)
+
+#         return x
+
+#################################################################################
+#################################################################################
+#################################################################################
+
+#################################################################################
+### Gated Fusion
+#################################################################################
+
+# class GatedFusion(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+
+#         self.obs_proj    = nn.Linear(obs_dim, 64)
+#         self.belief_proj = nn.Linear(belief_dim, 64)
+#         self.gate        = nn.Linear(obs_dim + belief_dim,  64)  # gate input = concatenated raw inputs
+    
+#     def forward(self, obs, belief, dimension):
+#         fo = torch.relu(self.obs_proj(obs))
+#         fb = torch.relu(self.belief_proj(belief))
+#         # compute gate g in (0,1)
+#         g = torch.sigmoid(self.gate(torch.cat([obs, belief],dim=dimension)))
+#         return g*fb + (1-g)*fo
+    
+class ObservationEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.obs_proj = nn.Linear(PPO_NETWORK_INPUT_SIZE, 64)
+
+    def forward(self, obs):
+        return torch.relu(self.obs_proj(obs))
+
+class BeliefEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.belief_proj = nn.Linear(PREDICTION_NETWORK_INPUT_SIZE, PREDICTION_NETWORK_OUTPUT_SIZE)
+
+    def forward(self, belief, dimension=0):
+        return torch.softmax(self.belief_proj(belief), dim=dimension)
+
+class ActorNetwork(nn.Module):
+    def __init__(self):
+        super(ActorNetwork, self).__init__()
+
+        self.obs_encoder = ObservationEncoder()
+        # self.gate = GatedFusion()
+
+
+        self.fusion_size = PPO_NETWORK_INPUT_SIZE + PREDICTION_NETWORK_OUTPUT_SIZE
+        self.hidden_size = 32
+        self.output_size = PPO_NETWORK_OUTPUT_SIZE
+
+        # Defining the fully connected layers
+        if USE_PREDICTION:
+            self.fc1 = nn.Linear(self.fusion_size, self.hidden_size)
+        else:
+            self.fc1 = nn.Linear(64, self.hidden_size)
+        self.dropout1 = nn.Dropout(0.3)
+        self.fc2 = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, obs, encoded_belief, dimension=0):
+        encoded_obs = self.obs_encoder(obs)
+        if USE_PREDICTION:
+            fused = torch.cat([obs, encoded_belief], dim=dimension)
+            # fused = self.gate(encoded_obs, encoded_belief, dimension)
+        else:
+            fused = encoded_obs
+        x = torch.relu(self.fc1(fused))
+        x = self.dropout1(x)
+        x = self.fc2(x)
+
+        return x
+    
+class CriticNetwork(nn.Module):
+    def __init__(self):
+        super(CriticNetwork, self).__init__()
+
+        self.obs_encoder = ObservationEncoder()
+        # self.gate = GatedFusion()
+
+
+        self.fusion_size = PPO_NETWORK_INPUT_SIZE + PREDICTION_NETWORK_OUTPUT_SIZE
+        self.hidden_size = 32
+        self.output_size = 1
+
+        # Defining the fully connected layers
+        if USE_PREDICTION:
+            self.fc1 = nn.Linear(self.fusion_size, self.hidden_size)
+        else:
+            self.fc1 = nn.Linear(64, self.hidden_size)
+        self.dropout1 = nn.Dropout(0.3)
+        self.fc2 = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, obs, encoded_belief, dimension=0):
+        encoded_obs = self.obs_encoder(obs)
+        if USE_PREDICTION:
+            fused = torch.cat([obs, encoded_belief], dim=dimension)
+            # fused = self.gate(encoded_obs, encoded_belief, dimension)
+        else:
+            fused = encoded_obs
+        x = torch.relu(self.fc1(fused))
+        x = self.dropout1(x)
+        x = self.fc2(x)
+
+        return x
+    
+class Network(nn.Module):
+    def __init__(self, device="cpu"):
+        super(Network, self).__init__()
+
+        self.actor = ActorNetwork()
+        self.critic = CriticNetwork()
+        self.belief_encoder = BeliefEncoder()
+
+        self.device = device
+
+    def forward(self, obs, belief, dimension=0):
+        encoded_belief = self.belief_encoder(belief)
+        actor = self.actor(obs, encoded_belief, dimension)
+        value = self.critic(obs, encoded_belief, dimension)
+
+        if USE_PREDICTION:
+            pred_action = torch.argmax(encoded_belief).unsqueeze(0)
+        else:
+            pred_action = torch.tensor([-1], device=self.device)
+
+        return actor, value, pred_action
+
+#################################################################################
+#################################################################################
+#################################################################################
---
+++ GPU/SINGLE/FUSION/PRN_FREQUENCY_HOPPING/fh_pattern.py
@@ -0,0 +1,59 @@
+from constants import *
+
+import random
+import torch
+
+class FH_Pattern:
+    def __init__(self, L = NUM_HOPS, genie = False, device = "cpu"):
+        self.L = L
+        self.device = device
+        self.genie = genie
+
+        # Initialize PRNG objects
+        random.seed(100)
+        self.seeds = [random.randint(0, 1000) for _ in range(NUM_SEEDS)]
+        # self.seeds = [616, 52, 218]
+        self.prng_objects = [random.Random(seed) for seed in self.seeds]
+
+        self.sequences = torch.tensor([], device=self.device)
+
+    # Generate a sequence of random numbers from all PRNG objects to keep them in sync,
+    # but only return the one corresponding to seed_index.
+    def generate_sequence(self):
+        self.sequences = torch.tensor([], device=self.device)
+        for prng in self.prng_objects:
+            seq = [prng.randint(0, NUM_CHANNELS - 1) for _ in range(self.L)]
+            self.sequences = torch.cat((self.sequences, torch.tensor(seq, device=self.device).unsqueeze(0)), dim=0)
+
+    def get_sequence(self, seed_index):
+        if self.L == 1 and self.genie == False:
+            # seed_index = seed_index.unsqueeze(0).long()
+            # return self.sequences[seed_index].squeeze(0).squeeze(0)
+            
+            # If L = 1, or NUM_HOPS = 1, the seed index is the same as the channel used for transmission
+            return seed_index
+        else:
+            return self.sequences[seed_index].squeeze(0)
+    
+    def get_seed(self, observed_channels):
+        for i, seq in enumerate(self.sequences):
+            if (seq == observed_channels).sum().item() > self.L / 2:
+                return torch.tensor([i], device=self.device)
+        return torch.tensor([-1], device=self.device)
+
+if __name__ == '__main__':
+    fh1 = FH_Pattern()
+    fh2 = FH_Pattern()
+    print("Seeds: ", fh1.seeds)
+    print("Time step 1")
+    print("fh1, seed 1: ", fh1.generate_sequence(0))
+    print("fh2, seed 2: ", fh2.generate_sequence(1))
+    print("Time step 2")
+    print("fh1, seed 2: ", fh1.generate_sequence(1))
+    print("fh2, seed 1: ", fh2.generate_sequence(0))
+    print("Time step 3")
+    print("fh1, seed 2: ", fh1.generate_sequence(1))
+    print("fh2, seed 1: ", fh2.generate_sequence(0))
+    print("Time step 4")
+    print("fh1, seed 1: ", fh1.generate_sequence(0))
+    print("fh2, seed 1: ", fh2.generate_sequence(0))
